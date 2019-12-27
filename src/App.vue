69c04fab96eca94e43ac33713ca392823f60d5e2
Aaron Pittenger <pittenger.aaron@gmail.com>
2019-12-26 22:27:33 -0500

    Added a navbar, bootstrap and icon

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,62 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div id='navbar'>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#">&#x1F440 <!-- Eyes Emoji --></b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">Link</b-nav-item>
+            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            </b-nav-form>
+
+            <b-nav-item-dropdown text="Lang" right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template v-slot:button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <div id='content'>
+      <img src="./assets/logo.png">
+      <h1>{{ msg }}</h1>
+      <h2>Essential Links</h2>
+      <ul>
+        <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+        <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+        <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+        <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+        <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      </ul>
+      <h2>Ecosystem</h2>
+      <ul>
+        <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+        <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+        <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+        <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -24,14 +65,14 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to glanc3'
     }
   }
 }
 </script>
 
 <style>
-#app {
+#content {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
